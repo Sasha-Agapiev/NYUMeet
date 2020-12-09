3038ed46f043190781dc1f535c62a87631fa1b66
Sasha Agapiev <57875787+Sasha-Agapiev@users.noreply.github.com>
2020-12-09 01:53:50 -0800

    Update process.php

--- process.php
+++ process.php
@@ -1,11 +1,34 @@
 /*Process for signup page*/
 <?php
-$mysqli = new mysqli('localhost', 'root', ' ', 'demo') or die(mysqli_error($mysqli));
-
+require_once "config.php";
+$username = $password = $confirmpassword = "";
+$username_err = $password_err = $confirmpassword_err = "";
 if(isset($_POST['submit'])){
-	$username= $_POST['username'];
-	$password= $_POST['password'];
-	$confirmpassword= $_POST['confirm'];
+	$inputusername= $_POST['username'];
+	$inputpassword= $_POST['password'];
+	$inputconfirmpassword= $_POST['confirm'];
+	if(empty($inputusername)){
+		$username_err = "Please enter a username.";
+	}
+	else{
+		$username = $inputusername;
+	}
+	if(empty($inputpassword)){
+		$password_err = "Please enter a password.";
+	}
+	else{
+		$password = $inputpassword;
+	}
+	if(empty($inputconfirmpassword)){
+		$confirmpassword_err = "Please confirm your password.";
+	}
+	else{
+		$confirmpassword = $inputconfirmpassword;
+	}
+	if($password != $confirmpassword){
+		$confirmpassword_err = "Make sure your passwords match.";
+	}
+	
 	$mysqli->query("INSERT INTO user (username, password) VALUES('$username', '$location')") or die($mysqli->error);
 }
 ?>
