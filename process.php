3a06538b939a81404458572b1373059716576e5b
Sasha Agapiev <57875787+Sasha-Agapiev@users.noreply.github.com>
2020-12-09 01:03:16 -0800

    Update process.php

--- process.php
+++ process.php
@@ -3,9 +3,9 @@
 $mysqli = new mysqli('localhost', 'root', ' ', 'demo') or die(mysqli_error($mysqli));
 
 if(isset($_POST['submit'])){
-	$username = $_POST['username'];
-	$password = $_POST['password'];
-	$confirmpassword = $_POST['confirm'];
+	$username= $_POST['username'];
+	$password= $_POST['password'];
+	$confirmpassword= $_POST['confirm'];
 	$mysqli->query("INSERT INTO user (username, password) VALUES('$username', '$location')") or die($mysqli->error);
 }
 ?>
