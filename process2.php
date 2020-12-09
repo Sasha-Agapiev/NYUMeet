7da603d00b2d790a7eacd4911b78db5a86cc6680
Sasha Agapiev <57875787+Sasha-Agapiev@users.noreply.github.com>
2020-12-09 01:38:04 -0800

    Update process2.php

--- process2.php
+++ process2.php
@@ -3,18 +3,33 @@
 $mysqli = new mysqli('localhost', 'root', ' ', 'demo') or die(mysqli_error($mysqli));
 
 if(isset($_POST['submit'])){
-	$username= $_POST['username'];
-	$password= $_POST['password'];
-	/*check if username in the user DB. If it is, check if the passwords match 	then allow them into the site, else reject + send some message */
-	$sql= "SELECT * FROM User WHERE username='".$username."'AND password='".$password."' limit 1"; 
-	$result=mysql_query($sql);
-	if(mysql_num_row($result)==1){
-		echo "Successfully logged into NYU Meet";
-		exit();
+	$inputusername= $_POST['username'];
+	$inputpassword= $_POST['password'];
+	if(empty($username)){
+		$username_err = "Please enter a username.";
 	}
 	else{
-		echo "Cannot Find User: Incorrect password or username inputted";
-		exit(); 
+		$username = $inputusername;
 	}
+	if(empty($password)){
+		$password_err = "Please enter a password.";
+	}
+	else{
+		$password = $inputpassword;
+	}
+	if(empty($username_err) && empty($password_err)){
+		/*check if username in the user DB. If it is, check if the passwords match then allow them into the site, else reject + send some message */
+		$sql= "SELECT * FROM User WHERE username='".$username."'AND password='".$password."' limit 1"; 
+		$result=mysql_query($sql);
+		if(mysql_num_row($result)==1){
+			echo "Successfully logged into NYU Meet";
+			exit();
+		}
+		else{
+			echo "Incorrect password or username inputted";
+			exit(); 
+		}
+	}
+	
 }
 ?>
