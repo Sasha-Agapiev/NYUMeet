14395ce7168384901338902dd9f2937136480eb2
Sasha Agapiev <57875787+Sasha-Agapiev@users.noreply.github.com>
2020-12-08 23:51:31 -0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,18 @@
+/* Config.php configurates the php to NYU Meet's MySQL database, to facilitate further   queries and inputs 
+CURRENTLY CONFIGURED to default host... need to update to include our MySQL information */
+
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'demo');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
+?>
