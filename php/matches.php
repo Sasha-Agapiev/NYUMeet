bd48f16697af9deb194fd36611cd85e59ad91809
Ephil012 <Etp5501@gmail.com>
2020-12-12 00:46:06 +0000

    Fixed match

--- php/matches.php
+++ php/matches.php
@@ -1,3 +1,11 @@
+<?php
+    include('processing/config.php');
+    session_start();
+    if(!isset($_SESSION['UserId'])){
+        header('Location: signin.php');
+        exit;
+    }
+?>
 <html>
     <head>
         <!-- Page Info -->
@@ -44,47 +52,26 @@
             <h1 class="title">Your Matches</h1>
             <div class="peopleSection">
                 <?php
-                include('processing/config.php');
-                session_start();
-                if(!isset($_SESSION['UserId'])){
-                    header('Location: signin.php');
-                    exit;
-                }
-                $ourUserId = $_SESSION['UserId'];
-
-                
-                $recordLimit = 10;
-                $offset = ($pageno-1) * $recordLimit;
-                
-                $query1 = $connection->prepare("SELECT FirstName, LastName, Username, Bio, Instagram, Snapchat FROM matches WHERE UserId2 = :thisUserId INNER JOIN Users ON matches.UserId1 = Users.:thisUserId");
-                $query1->bindParam("thisUserId", ourUserId, PDO::PARAM_INT);
-                $result1 = $query1->execute();
-                /* Create the table*/
-                while($findRow = $query1->fetch(PDO::FETCH_ASSOC)){
+                    $matchesQuery = $connection->prepare("SELECT FirstName, LastName, Username, Bio, Instagram, Snapchat FROM Matches INNER JOIN Users ON Matches.UserId2 = Users.UserId WHERE Matches.UserId1 = :UserId");
+                    $matchesQuery->bindParam("UserId", $_SESSION['UserId'], PDO::PARAM_INT);
+                    $matchesResult = $matchesQuery->execute();
+                    while ($findRow = $matchesQuery->fetch(PDO::FETCH_ASSOC)){
                     ?>
-                    <div class="personContainer">
-                        <img class="personImage" src="https://images.unsplash.com/photo-1584799235813-aaf50775698c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=zheka-boychenko-vPkTWyTgk8E-unsplash.jpg"/>
-                        <div class="personInfoContainer">
-                            <div class="titleContainer">
-                                <h2 class="itemTitle"><?php echo $findRow['FirstName']?> <?php echo $findRow['LastName'] ?></h2>
+                        <div class="personContainer">
+                            <img class="personImage" src="https://images.unsplash.com/photo-1584799235813-aaf50775698c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=zheka-boychenko-vPkTWyTgk8E-unsplash.jpg"/>
+                            <div class="personInfoContainer">
+                                <div class="titleContainer">
+                                    <h2 class="itemTitle"><?php echo $findRow['FirstName']?> <?php echo $findRow['LastName'] ?></h2>
+                                </div>
+                                <p class="personBio"><?php echo $findRow['Bio']?></p>
+                                <h3 class="itemTitle">Contact Info</h3>
+                                <p class="personBio"><?php echo 'Snapchat:', $findRow['Snapchat']?><br>
+                                    <?php echo 'Instagram:', $findRow['Instagram']?><br>
+                                    <?php echo 'Zoom:', $findRow['Zoom']?>
+                                </p>
                             </div>
-                            <p class="personBio"><?php echo $findRow['Bio']?></p>
-                            <h3 class="itemTitle">Contact Info</h3>
-                            <p class="personBio"><?php echo 'Snapchat:', $findRow['Snapchat']?><br>
-                                <?php echo 'Instagram:', $findRow['Instagram']?><br>
-                                <?php echo 'Zoom:', $findRow['Zoom']?>
-                            </p>
                         </div>
-                    </div>
-                /* Pagination */
-                <ul style="list-style-type:none;">
-                    <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
-                        <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>">Prev</a>
-                    </li>
-                    <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
-                        <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>">Next</a>
-                    </li>
-                </ul>
+                    <?php } ?>
             </div>
         </div>
     </body>
